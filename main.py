3203b13651955247fdb355a1005adf3b9b2e63d5
Agastya Patri <patriagastya5@gmail.com>
2022-11-27 20:32:39 +0530

    creating model

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from corpus.song import Song
+from src.model import Model 
+import os 
+
+"""
+    1. Importing the data from a OklamAI/corpus/lyrics/KDOT.txt
+
+    ~ corpus is a list of all sentences. 
+"""
+path = "OklamAI/corpus/lyrics/"
+ 
+with open(os.path.join(path, "KDOT.txt")) as file:
+    corpus =  file.read().split("\n")
+
+
+
+
+
 
 
---
+++ src/model.py
@@ -0,0 +1,15 @@
+"""
+    Defining the model that will be used for text generation 
+"""
+import torch 
+import torch.nn as nn 
+import numpy as np 
+
+
+class Model(nn.Module):
+    pass 
+
+
+
+if __name__ == "__main__":
+    pass 
